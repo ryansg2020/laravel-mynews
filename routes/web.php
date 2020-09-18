e2b782f19fffd7ed2f2594e85d57a323c7a98ec2
”ryansg” <mynadc@gmail.com>
2020-09-18 17:42:19 +0000

    hw submission9

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-});
-
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin' , 'middlewar' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create'); # 追記
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
